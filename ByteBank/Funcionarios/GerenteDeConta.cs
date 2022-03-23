03b49d354d7dacb6f62a0a3abdb744c7b42d02b6
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-03-23 12:40:06 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Funcionarios/GerenteDeConta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class GerenteDeConta : Funcionario
+    {
+        public string Senha { get; set; }
+        public GerenteDeConta(string cpf) : base(4000, cpf)
+        {
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.05;
+        }
+
+        public override double GetBonificacao()
+        {
+            return Salario * 0.25;
+        }
+        public bool Autenticar(String senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
---
+++ ByteBank/Sistemas/SistemaInterno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Funcionarios;
+
+namespace ByteBank.Sistemas
+{
+    internal class SistemaInterno
+    {
+        public bool Logar(Funcionario diretor, String senha)
+        {
+            bool userAutenticador = diretor.Autenticar(senha);
+            if (userAutenticador)
+            {
+                Console.WriteLine("<BEM VINDO AO SISTEMA DA BYTEBANK>");
+                return true;
+            }
+            Console.WriteLine("Não autenticado!");
+            return false;
+        }
+        public bool Logar(GerenteDeConta GConta, String senha)
+        {
+            bool userAutenticador = GConta.Autenticar(senha);
+            if (userAutenticador)
+            {
+                Console.WriteLine(" BEM VINDO AO SISTEMA DA BYTEBANK ");
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+
+}
